d478b073190f382061be503706892c895b1605c0
ericb <eric_bastarache@hotmail.com>
2017-02-21 17:57:12 -0500

    Moved routes file

---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+
+export const AppRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: ErrorComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
