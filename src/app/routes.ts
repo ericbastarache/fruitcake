a062dc02172cc1dba6547a9cff1015b9785bf979
ericb <eric_bastarache@hotmail.com>
2017-02-21 22:58:03 -0500

    Miscellaneous update

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,5 @@
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
