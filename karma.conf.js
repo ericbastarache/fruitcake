40f72dd8fc2161cdcdcda9de3b13ce5fb736feec
ericb <eric_bastarache@hotmail.com>
2017-02-23 00:12:47 -0500

    Final test of chrome

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  var configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
@@ -45,5 +45,9 @@ module.exports = function (config) {
       }
     },
     singleRun: false
-  });
+  };
+  if(process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+  config.set(configuration);
 };
