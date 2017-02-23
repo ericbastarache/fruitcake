3bee9eafc3caf0dfce55ce50afd3b99371e0a3ca
ericb <eric_bastarache@hotmail.com>
2017-02-22 23:53:35 -0500

    Added chrome back

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-remap-istanbul'),
       require('@angular/cli/plugins/karma')
@@ -37,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Chrome','Firefox'],
     singleRun: false
   });
 };
