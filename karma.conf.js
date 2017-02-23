b07f8f2b78cc9aca79187a6e7a789e35950d8dfe
ericb <eric_bastarache@hotmail.com>
2017-02-22 23:56:55 -0500

    Removed chrome again

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
       require('karma-remap-istanbul'),
       require('@angular/cli/plugins/karma')
@@ -38,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome','Firefox'],
+    browsers: ['Firefox'],
     singleRun: false
   });
 };
