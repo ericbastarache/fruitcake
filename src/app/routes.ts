2887ff95b8eede552372741807df264418cefec5
ericb <eric_bastarache@hotmail.com>
2017-02-24 14:57:31 -0500

    updated routes to reflect component additions

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,15 +1,24 @@
 import { RouterModule, Routes } from '@angular/router';
 
+
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const APP_ROUTES: Routes = [
   { path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: '**', component: ErrorComponent }
 ];
